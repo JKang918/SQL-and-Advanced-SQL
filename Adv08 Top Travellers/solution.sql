e8c1a9b799c1281e60cb4eb3de47c5b6e64b696b
JKang918 <jihyun.kang.0110@gmail.com>
2024-06-29 22:51:58 +0900

    initial commit (solution)

---
+++ Adv08 Top Travellers/solution.sql
@@ -0,0 +1,9 @@
+SELECT 
+    name, IFNULL(SUM(distance),0) AS travelled_distance
+FROM
+    Users u
+    LEFT JOIN Rides r ON u.id = r.user_id
+GROUP BY 
+    u.id #not name (different users with the same name)
+ORDER BY 
+    travelled_distance DESC, name ASC;
